6df406b1905302aa74a19344b4514e18b76d0117
Zlatin Hristov <zlatin.hristov@softavis.com>
2022-03-02 18:56:31 +0200

    Increase maximumError budget

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true,
+  s3_region: 'eu-central-1',
+  s3_domain: '//angularpluss3.s3.eu-central-1.amazonaws.com/',
+  s3_access_key_id: 'AKIAXDTBUJTVDPMCASMO',
+  s3_secret_access_key: '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',
+  cloud_image_token: 'afxpjqnfrq',
 };
